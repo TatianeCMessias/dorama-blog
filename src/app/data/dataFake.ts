8fa562df3e9bfd530a166abd260396fe2940646c
Tatiane Messias <tatianem600@gmail.com>
2025-03-08 21:47:10 -0300

    Blog Concluído

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "PRETENDENTE SURPRESA É UM DOS MELHORES DORAMAS DA NETFIX",
+    "description": "Nesse dorama você vai se encantar com a temática de comédia romântica.",
+    "photoCover":"https://sodoramasoficial.com.br/wp-content/uploads/2024/05/pretendente-surpresa-dorama.avif"
+  },
+  {
+    "id":"2",
+    "title": "Melhor Dorama de Época na Netflix",
+    "description": "Para os amantes de dramas históricos, Alquimia das Almas é sem dúvida o melhor.",
+    "photoCover":"https://sodoramasoficial.com.br/wp-content/uploads/2024/12/Alquimia-das-Almas-1-1536x827.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Dorama de Viagem: Explorando Destinos e Culturas",
+    "description": "“Goblin” é uma série de fantasia que conta a história de um ser imortal em busca de sua noiva humana.",
+    "photoCover":"https://sodoramasoficial.com.br/wp-content/uploads/2024/11/goblin-dorama-cena-dorama-coreano-romance-1.jpeg"
+  },
+  {
+  "id":"4",
+    "title": "Melhor Dorama Clássico",
+    "description": "Uma Advogada Extraordinária conta a história de Woo Young-woo, uma advogada com autismo que trabalha em um grande escritório de advocacia.",
+    "photoCover":"https://lazersemfronteiras.com.br/wp-content/uploads/2024/11/Uma-Advogada-Extraordinaria.webp"
+  }
+]
