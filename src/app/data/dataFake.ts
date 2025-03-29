63f1bfc84c2dfd0ec2fb97fc24e794de8f2c4627
Tatiane Messias <tatianem600@gmail.com>
2025-03-29 17:56:42 -0300

    Atualizando as imagens

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,32 +1,43 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "PRETENDENTE SURPRESA É UM DOS MELHORES DORAMAS DA NETFIX",
-    "description": "Nesse dorama você vai se encantar com a temática de comédia romântica.",
-    "photoCover": "/assets/imgs/PRETENDENTE-SURPRESAA.png"
+    id: '1',
+    title: 'PRETENDENTE SURPRESA É UM DOS MELHORES DORAMAS DA NETFLIX',
+    description:
+      'Nesse dorama você vai se encantar com a temática de comédia romântica.',
+    photoCover: '/assets/imgs/PRETENDENTE-SURPRESAA.png',
   },
   {
-    "id":"2",
-    "title": "Melhor Dorama de Época na Netflix",
-    "description": "Para os amantes de dramas históricos, Alquimia das Almas é sem dúvida o melhor.",
-    "photoCover":"/assets/imgs/Alchemy-of-Souls.png"
+    id: '2',
+    title: 'AMANHÃ É UM DOS MELHORES DORAMAS DE FANTASIA DA NETFLIX',
+    description: 'Esse dorama mistura fantasia e drama de forma emocionante.',
+    photoCover: '/assets/imgs/Amanha-Tomorrow.png',
   },
   {
-    "id":"3",
-    "title": "Dorama de Viagem",
-    "description": "“Goblin” é uma série de fantasia que conta a história de um ser imortal em busca de sua noiva humana.",
-    "photoCover":"/assets/imgs/-Goblin.png"
+    id: '3',
+    title: 'Melhor Dorama de Época na Netflix',
+    description:
+      'Para os amantes de dramas históricos, Alquimia das Almas é sem dúvida o melhor.',
+    photoCover: '/assets/imgs/Alchemy-of-Souls.png',
   },
   {
-  "id":"4",
-    "title": "Melhor Dorama Clássico",
-    "description": "Uma Advogada Extraordinária conta a história de Woo Young-woo, uma advogada com autismo que trabalha em um grande escritório de advocacia.",
-    "photoCover":"/assets/imgs/uma-advogada-extraordinaria1.png"
+    id: '4',
+    title: 'Dorama de Viagem',
+    description:
+      '“Goblin” é uma série de fantasia que conta a história de um ser imortal em busca de sua noiva humana.',
+    photoCover: '/assets/imgs/Goblin.png',
   },
   {
-    "id":"5",
-      "title": "Melhor Dorama de Fantasia",
-      "description": "W Dois Mundos.",
-      "photoCover":"/assets/imgs/drama2.png"
-    }
-]
+    id: '5',
+    title: 'Melhor Dorama Clássico',
+    description:
+      'Uma Advogada Extraordinária conta a história de Woo Young-woo, uma advogada com autismo que trabalha em um grande escritório de advocacia.',
+    photoCover: '/assets/imgs/uma-advogada-extraordinaria1.png',
+  },
+  {
+    id: '6',
+    title: 'Melhor Dorama de Fantasia',
+    description:
+      'W Dois Mundos é um dorama inovador que mistura realidade e ficção.',
+    photoCover: '/assets/imgs/w-two-worlds.png',
+  },
+];
