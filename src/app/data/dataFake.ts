0355187d0f5c76a948a2ba8463484a52418924e9
Tatiane Messias <tatianem600@gmail.com>
2025-03-29 19:19:23 -0300

    Atualização no dataFake e no Home

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,47 +3,47 @@ export const dataFake = [
     id: '1',
     title: 'PRETENDENTE SURPRESA É UM DOS MELHORES DORAMAS DA NETFLIX',
     description:
-      'Nesse dorama você vai se encantar com a temática de comédia romântica.',
+      'Ha-ri vai a um encontro às cegas se passando pela amiga e pronta para dispensar o pretendente. Mas o plano dá errado, e ele acaba fazendo uma proposta para ela.',
     photoCover: '/assets/imgs/PRETENDENTE-SURPRESAA.png',
   },
   {
     id: '2',
     title: 'AMANHÃ É UM DOS MELHORES DORAMAS DE FANTASIA DA NETFLIX',
-    description: 'Esse dorama mistura fantasia e drama de forma emocionante.',
+    description: 'Um jovem se torna meio humano e meio espírito por acidente e entra para uma equipe de anjos da morte para realizar missões especiais.',
     photoCover: '/assets/imgs/Amanha-Tomorrow.png',
   },
   {
     id: '3',
     title: 'AMOR OCULTO É MELHOR DORAMA CHINÊS DA NETFLIX',
-    description: 'Esse dorama fala de crescimento profissional e pessoal.',
+    description: 'Uma jovem (Zhao Lusi) nutre um amor platônico pelo amigo do irmão mais velho (Chen Zheyuan). No momento, ele não passa disso: amigo do irmão. Por anos ela segue apaixonada sem saber exatamente o que o rapaz sente.',
     photoCover: '/assets/imgs/amor-oculto-s.png',
   },
   {
     id: '4',
     title: 'Melhor Dorama de Época e Fantasia na Netflix',
     description:
-      'Para os amantes de dramas históricos, Alquimia das Almas é sem dúvida o melhor.',
+      'Ambientada em um país fictício chamado Daeho, a série é sobre o amor e o crescimento de jovens magos enquanto eles superam seus destinos distorcidos devido a um feitiço mágico proibido conhecido como "Alquimia das Almas", que permite que as almas troquem de corpo.',
     photoCover: '/assets/imgs/Alchemy-of-Souls.png',
   },
   {
     id: '5',
     title: 'Dorama de Romance e Fantasia',
     description:
-      '“Goblin” é uma série de fantasia que conta a história de um ser imortal em busca de sua noiva humana.',
+      'Kim Shin (Gong Yoo) é um goblin imortal responsável por proteger as almas dos humanos. Ele está decidido a perder sua imortalidade, mas um goblin só pode pôr fim à sua vida se ele se casar com uma humana.',
     photoCover: '/assets/imgs/Goblin.png',
   },
   {
     id: '6',
     title: 'Melhor Dorama Clássico',
     description:
-      'Uma Advogada Extraordinária conta a história de Woo Young-woo, uma advogada com autismo que trabalha em um grande escritório de advocacia.',
+      'Uma jovem autista brilhante é contratada por um grande escritório de advocacia. Ela possui um QI alto e uma memória impressionante, mas tem dificuldade com interações cotidianas.',
     photoCover: '/assets/imgs/uma-advogada-extraordinaria1.png',
   },
   {
     id: '7',
     title: 'Melhor Dorama de Fantasia',
     description:
-      'W Dois Mundos é um dorama inovador que mistura realidade e ficção.',
+      'A cirurgiã Oh Yeon-Joo é puxada para o universo paralelo de uma história em quadrinhos chamada W, que foi criado por seu pai. Lá, ela se envolve em um mistério de assassinato e uma história de amor impossível.',
     photoCover: '/assets/imgs/w-two-worlds.png',
   },
 ];
